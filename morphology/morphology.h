2345409dce9ac7cf4c593a903115123edb028e64
jakiejoe <56106412+wangjie200@users.noreply.github.com>
2022-04-27 13:17:26 +0800

    Delete morphology directory

--- morphology/morphology.h
+++
@@ -1,6 +0,0 @@
-#include<vector>
-#include "morphology/bean/Token.h"
-void getNextChar();
-void ungentNextChar();
-void reservedLookUp();
-vector<Token> getTokenList();
--- morphology/util.cpp
+++
@@ -1,3 +0,0 @@
-#include<vector>
-
-void ChianToFile();
